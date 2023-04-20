2f9641be6486b45babe462aee9a8e328b3f0195f
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-20 23:33:23 +0300

    rew

--- backend/foodgram/api/pagination.py
+++
@@ -1,9 +0,0 @@
-from django.core.paginator import Paginator
-from rest_framework.pagination import PageNumberPagination
-
-
-class LimitPageNumberPaginator(PageNumberPagination):
-    '''Pagination with others parameters.'''
-    django_paginator_class = Paginator
-    page_query_param = 'page'
-    page_size_query_param = 'limit'
