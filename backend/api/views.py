37b0c3999ff0b9d04c298ee67ab95c221ec4d136
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-26 08:30:05 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,10 +5,8 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import (
-                                        SAFE_METHODS, IsAuthenticated,
-                                        AllowAny, IsAuthenticatedOrReadOnly
-                                        )
+from rest_framework.permissions import (SAFE_METHODS, IsAuthenticated,
+                                        AllowAny, IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
