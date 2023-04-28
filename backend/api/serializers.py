abeb2d58baf874317844ec02ee3c48ac2889214e
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-28 10:54:24 +0300

    4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import CharField
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
