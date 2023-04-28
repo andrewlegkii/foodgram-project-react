763255ecf6b3110d90ff88ff4511216d44ecb0b3
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-28 11:50:12 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from datetime import datetime as dt
+from django.core.handlers.wsgi import WSGIRequest
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import F
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import (SAFE_METHODS, IsAuthenticated,
@@ -19,6 +21,8 @@ from .permissions import IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
                           RecipeShortSerializer, RecipeWriteSerializer,
                           TagSerializer)
+
+from foodgram.settings import DATE_TIME_FORMAT
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -100,30 +104,31 @@ class RecipeViewSet(ModelViewSet):
         detail=False,
         permission_classes=[IsAuthenticated]
     )
-    def download_shopping_cart(self, request):
-        """Метод для скачивания списка покупок."""
-        user = request.user
-        if not user.shopping_cart.exists():
+    @action(methods=('get',), detail=False)
+    def download_shopping_cart(self, request: WSGIRequest) -> Response:
+        user = self.request.user
+        if not user.carts.exists():
             return Response(status=HTTP_400_BAD_REQUEST)
-        ingredients = IngredientInRecipe.objects.filter(
-            recipe__shopping_cart__user=request.user
-        ).values(
-            'ingredient__name',
-            'ingredient__measurement_unit'
-        ).annotate(amount=Sum('amount'))
-        today = datetime.today()
-        shopping_list = (
-            f'Список покупок для: {user.get_full_name()}\n\n'
-            f'Дата: {today:%Y-%m-%d}\n\n'
-        )
-        shopping_list += '\n'.join([
-            f'- {ingredient["ingredient__name"]} '
-            f'({ingredient["ingredient__measurement_unit"]})'
-            f' - {ingredient["amount"]}'
-            for ingredient in ingredients
-        ])
-        shopping_list += f'\n\nFoodgram ({today:%Y})'
+
         filename = f'{user.username}_shopping_list.txt'
+        shopping_list = [
+            f'Список покупок для:\n\n{user.first_name}\n'
+            f'{dt.now().strftime(DATE_TIME_FORMAT)}\n'
+        ]
+
+        ingredients = Ingredient.objects.filter(
+            recipe__recipe__in_carts__user=user
+        ).values(
+            'name',
+            measurement=F('measurement_unit')
+        ).annotate(amount=Sum('recipe__amount'))
+
+        for ing in ingredients:
+            shopping_list.append(
+                f'{ing["name"]}: {ing["amount"]} {ing["measurement"]}'
+            )
+        shopping_list.append('\nПосчитано в Foodgram')
+        shopping_list = '\n'.join(shopping_list)
         response = HttpResponse(
             shopping_list, content_type='text.txt; charset=utf-8'
         )
