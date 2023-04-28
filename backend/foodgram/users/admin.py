4445cc1ee4452c3f62555cfe29d44b783ec82a6c
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-28 21:41:44 +0300

    4

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'username', 'pk', 'email', 'password', 'first_name', 'last_name',
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ('id', 'username', 'email')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal Info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    list_editable = ('password', )
-    list_filter = ('username', 'email')
-    search_fields = ('username', 'email')
-    empty_value_display = '-пусто-'
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
 
 
 @admin.register(models.Subscribe)
 class SubscribeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'author')
-    list_editable = ('user', 'author')
+    list_display = ('id', 'user', 'author')
+    list_display_links = ('id', 'user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = ('user__username', 'author__username')
     empty_value_display = '-пусто-'
