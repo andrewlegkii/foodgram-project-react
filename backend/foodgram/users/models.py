c9b41bf7a1d9aefef267f8b7c698bcb835256c48
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-28 23:37:36 +0300

    rew

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -38,6 +38,9 @@ class User(AbstractUser):
         blank=False,
         null=False,
     )
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     class Meta:
         ordering = ['id']
